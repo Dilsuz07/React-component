7d3287b3cdb5324a2de403463abffe26ed6c7b7a
Dilsuz07 <dilsuz25012303@gmail.com>
2022-03-27 08:23:53 +0500

    'button'

--- src/About.js
+++ src/About.js
@@ -1,15 +1,32 @@
 import React from "react";
 import Home from "./Home";
+import img from './image/fon_3.jpg'
+
+let header1 = document.querySelector('.header1')
+
+
+
+// btn.addEventListener('click', () => {
+//   header1.style.display = 'none'
+// })
 
 const About = () => {
     return (
         <>
-            <h1>
+            <h1 className="header1">
               Create a New React App
             </h1>
+            <button className="delete" onClick={()=> {
+              header1.style.display = 'none'
+            }}>Delete</button>
             <p>
+            
+            <button className="insert" onClick={() => {
+               header1.style.display = 'block'
+            }}>Insert</button>
               When starting a React project, a simple HTML page with script tags might still be the best option. It only takes a minute to set up! As your application grows, you might want to consider a more integrated setup. There are several JavaScript toolchains we recommend for larger applications. Each of them can work with little to no configuration and lets you take full advantage of the rich React ecosystem. Learn how.
             </p>
+            <img src={img}></img>
             <Home
                 hello = 'Hello'
                 world = 'World'
